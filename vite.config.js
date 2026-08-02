9bce72948b250d0298c3ebc535beab26a4691065
GlennDiamond1104 <gleb_diamondh01@unicah.edu>
2026-08-02 17:27:30 -0600

    added in important factor 2

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,21 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  appType: 'npm',
+  base: process.env.DEPLOY_BASE_URL ?? '/',
   plugins: [tailwindcss(), react()],
 })
+
+
+/*
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  appType: 'npm',
+  base: '/pokemon_list_work/', // <--- Update it to match your exact repo name
+  plugins: [tailwindcss(), react()],
+})
+  */
